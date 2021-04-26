0f3aa88fc15501f8da5902c88efa90d25a18519a
NRaj6 <73037053+NRaj6@users.noreply.github.com>
2021-04-26 10:39:03 -0400

    Update submissionformjscript.js

--- firebase with form/submissionformjscript.js
+++ firebase with form/submissionformjscript.js
@@ -6,13 +6,13 @@ var config = {
     storageBucket: "sample1-7cd2a.appspot.com",
   };
 
-// Initialize your Firebase app
+// Initialize Firebase app
 firebase.initializeApp(config);
 
-// Reference to the submissions object in your Firebase database
+// Reference to the submissions object in Firebase database
 var submissions = firebase.database().ref("submission");
 
-// Save a new rsubmission to the database, using the input in the form
+// Save a new submission to the database, using the input in the form
 var sub = function () {
 
   // Get input values from each of the form elements
@@ -25,7 +25,7 @@ var sub = function () {
   var sourceName = $("#sourcevar").val();
   var pic = $("#subimage").val();
 
-  // Push a new recommendation to the database using those values
+  // Push a new submisison object to the database using those values
   submissions.push({
     "date in history": title,
     "person": person,
@@ -39,14 +39,14 @@ var sub = function () {
 };
 
 // When the window is fully loaded, call this function.
-// Note: because we are attaching an event listener to a particular HTML element
-// in this function, we can't do that until the HTML element in question has
+// because we are attaching a submission to a particular HTML element
+// in this function, we can't do that until the HTML element connected has
 // been loaded. Otherwise, we're attaching our listener to nothing, and no code
 // will run when the submit button is clicked.
 $(window).load(function () {
 
-  // Find the HTML element with the id recommendationForm, and when the submit
-  // event is triggered on that element, call submitRecommendation.
+  // Find the HTML element with the id submissionForm and when the submit
+  // event is triggered on that element, call sub.
   $("#submissionForm").submit(sub);
 
 });
