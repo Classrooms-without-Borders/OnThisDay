a43ec2f89880e1d7fc2c567d719ae9ab1fec302a
marcelomorales <lmoral10@jhu.edu>
2021-04-27 20:58:41 -0400

    retrieving data from firestore, printinng out in react

--- view/src/App.js
+++ view/src/App.js
@@ -1,7 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import firebase from './Firestore'
+//import firebase from 'firebase/app';
+import 'firebase/firestore';
+
 
 function App() {
+
+    let db = firebase.firestore();
+    let docRef = db.collection("classes").doc("CTodKLrFNrf3b8k4E6X1");
+    //let document;
+
+    docRef.get().then((doc) => {
+        if (doc.exists) {
+            console.log("Document data:", doc.data());
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch((error) => {
+        console.log("Error getting document:", error);
+    });
+
+//}
   return (
     <div className="App">
       <header className="App-header">
@@ -12,6 +33,8 @@ function App() {
        
       </header>
     </div>
+
+     
   );
 
 
