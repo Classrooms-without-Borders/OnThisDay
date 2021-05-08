dcf4a5f8034c0fb6686b49041c64a597e3a0ba27
marcelomorales <lmoral10@jhu.edu>
2021-05-07 20:34:32 -0400

    header is now horizaontal

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-import { Navbar,  Nav, NavbarToggler, Collapse, NavItem } from 'reactstrap';
+import { Navbar,  Nav, Collapse, NavItem } from 'reactstrap';
 import {NavLink} from 'react-router-dom';
 import '../styling/Header.css';
+import SearchIcon from '@material-ui/icons/Search';
 
 class Header extends Component{
 
@@ -11,8 +12,7 @@ class Header extends Component{
             isNavOpen: false,
             isModalOpen: false
         };
-        this.toggleNav = this.toggleNav.bind(this);
-        this.toggleModal = this.toggleModal.bind(this);
+ 
     }
 
     toggleNav(){
@@ -27,11 +27,16 @@ class Header extends Component{
         });
     }
 
+    handleClick() {
+        console.log("Search icon works"); //logs Observer class instance
+      }
+
 
     render(){
 
         const styleSheet = {
             color: "white"
+           
         }
 
         const active={
@@ -40,41 +45,48 @@ class Header extends Component{
             paddingBottom: '4px', 
             font: 'Roboto',
             background:'red',
-        
+    
         }
 
         return (
             
 
-
             <Navbar className="color">
                 <div className="container">
 
-                
-                
-                <Collapse isOpen={this.state.isNavOpen} navbar>
-                    <Nav navbar className="ml-auto">
+            
+             
 
-                    <div class="row justify-content-start">
+                    <div class="flexbox-container">
+                        <div>
+                            <NavItem>
+                                <NavLink className="Nav-link" to='/home' activeClassName='active' activeStyle={active} style={styleSheet}> Home</NavLink>
+                            </NavItem>
+                        </div>
 
-                        <NavItem>
-                            <NavLink className="Nav-link" to='/home' activeClassName='active' activeStyle={active} style={styleSheet}> Home</NavLink>
-                        </NavItem>
+                        <div>
+                            <NavItem>
+                                <NavLink className="Nav-link" to='/gallery' activeClassName='active' activeStyle={active} style={styleSheet}>Gallery</NavLink>
+                            </NavItem>
+                        </div>
+                        <div>
+                            <NavItem>
+                                <NavLink className="Nav-link" to='/submit' activeClassName='active' activeStyle={active} style={styleSheet}>Submit</NavLink>
+                            </NavItem>
+                        </div>
+                        <div>
+                            <NavItem>
+                                <NavLink className="Nav-link"  to='/about' activeClassName='active' activeStyle={active} style={styleSheet}> About</NavLink>
+                            </NavItem>
+                        </div>
 
-                        <NavItem>
-                            <NavLink className="Nav-link" to='/gallery' activeClassName='active' activeStyle={active} style={styleSheet}>Gallery</NavLink>
-                        </NavItem>
-
-                        <NavItem>
-                            <NavLink className="Nav-link" to='/submit' activeClassName='active' activeStyle={active} style={styleSheet}>Submit</NavLink>
-                        </NavItem>
-
-                        <NavItem>
-                            <NavLink className="Nav-link"  to='/about' activeClassName='active' activeStyle={active} style={styleSheet}> About</NavLink>
-                        </NavItem>
-                        </div>   
-                    </Nav>
-                </Collapse>
+                        <div class="right-align">
+                            <SearchIcon color="secondary" fontSize="large" onClick={this.handleClick} />
+                        </div>
+                           
+                    </div>   
+                    
+           
                 </div>
             </Navbar>
             
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,8 @@
 import React from "react";
 import '../styling/About.css'
 import BigCard from '../components/BigCard';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 //import { makeStyles } from '@material-ui/core/styles';
 
 
@@ -21,7 +23,23 @@ class About extends React.Component {
 				<React.Fragment>
 				<h2>Welcome to about page</h2>
 					<BigCard/>
+
+					<Typography variant="h1" component="h2" >
+						On this Day in History
+      				</Typography>
+					  <Typography variant="subtitle1" gutterBottom>
+					  A brief description of the project. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus egestas massa at aliquam suscipit. Integer ut laoreet turpis. Phasellus orci leo, dapibus sit amet consequat sed, tincidunt in augue.
+      </Typography>
+					<Button class="btn-design">
+        				Explore the Gallery
+      				</Button>
+					<Button variant="contained" color="primary">
+        				Submit an Entry
+      				</Button>
 				</React.Fragment>
+				
+
+
 					
 				
 			);
