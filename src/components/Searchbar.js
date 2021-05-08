4827f4f71e2d0d382b9c4ea2555b220ee01a1af6
anna-xing <anna.xing.22@gmail.com>
2021-05-07 20:33:34 -0400

    Created basic searchbar

---
+++ src/components/Searchbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { constants } from '../styling/Constants.js';
+import '../styling/Searchbar.css';
+
+import { TextInput } from '../components/Input';
+import { StyledButton } from '../components/Button';
+
+export function Searchbar({ location=null, date=null }) {
+    return (
+        <div class='searchbar'>
+            <div>
+                <p>What happened in </p>
+                <TextInput label='City, Country' />
+                <p> on </p>
+                <TextInput label='Date' />
+                <p>?</p>
+                <div class='searchbar-btn-div'>
+                    <StyledButton color={constants.color.dark}>
+                        Search
+                    </StyledButton>
+                    <a>Advanced Search</a>
+                </div>
+            </div>
+        </div>
+    )
+}
