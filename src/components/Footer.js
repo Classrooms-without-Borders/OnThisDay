7f9e4291cbc69ad418f537aa8a28f6eec97d7902
marcelomorales <lmoral10@jhu.edu>
2021-05-12 18:14:13 -0400

    finished footer accoring to client

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import logo from '../images/cwb-logo-reverse-w-tagline 1.png';
+import programInfo from '../images/jewish-federation-pitt.png';
 import facebook from '../images/facebook-logo.png';
-import twitter from '../images/twitter-logo.png';
+import insta from '../images/insta-logo.png';
 import '../styling/Footer.css';
 
 
@@ -30,8 +31,10 @@ function Footer(props) {
     <div className="footer" style={{backgroundColor:'#222629', padding:'-10%'}}>
       <div class="flexbox-container">
         <div> <img src={logo}  alt="logo"  /> </div>
-        <div> <p style={{color:'white'}}>© Copyright 2021. Classrooms Without Borders. All rights reserved.Contact Us</p> </div>
-        <div> <p style={{color:'white'}}> <a href="https://classroomswithoutborders.org/" target="_blank" rel="noopener noreferrer">Privacy Statement</a> </p> </div>
+        <div> <img src={programInfo}  alt="logo"  /> </div>
+        <div> <p style={{color:'white'}}>Classrooms Without Borders is an independently funded program of the Jewish Federation of Greater Pittsburgh</p> </div>
+        <div> <p style={{color:'white'}}> <a href="https://classroomswithoutborders.org/" target="_blank" rel="noopener noreferrer">Website</a> </p> </div>
+        <div> <p style={{color:'white'}}> <a href="https://classroomswithoutborders.org/donate/" target="_blank" rel="noopener noreferrer">Donate</a> </p> </div>
         <div>
           <a href="https://www.facebook.com/classroomswithoutborders">
             <img src={facebook}  alt="logo"  />
@@ -39,8 +42,8 @@ function Footer(props) {
           </div>
         
           <div>
-          <a href="https://twitter.com/CWBPgh">
-            <img src={twitter}  alt="logo"  />
+          <a href="https://www.instagram.com/classroomswithoutborders/">
+            <img src={insta}  alt="logo"  />
           </a>
           </div>
         </div>
