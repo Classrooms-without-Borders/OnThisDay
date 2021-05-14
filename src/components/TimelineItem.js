9fe9a735ec0fdb1e6bd9dbe6a44417748e27740b
marcelomorales <lmoral10@jhu.edu>
2021-05-14 17:08:12 -0400

    set routers for gallery years

--- src/components/TimelineItem.js
+++ src/components/TimelineItem.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import './timelineYears.js';
 import '../styling/TimelineItem.css'
+import Button from '@material-ui/core/Button';
+// routers
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+   // Redirect
+  } from "react-router-dom";
+
 
 
 const TimelineItem = ({data}) => (
     <div className="timeline-item">
-        <div className="timeline-item-content">
-           {data.year}
-        </div>   
+        <Button component={Link} to={{pathname: `gallery/${data.year}`}} color="primary">
+        
+            <div className="timeline-item-content">
+                {data.year}
+            </div>   
+        </Button>
     </div>
 );
 
