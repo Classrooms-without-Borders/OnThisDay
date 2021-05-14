e27454427974fc483b8dce0615f15c66e26ca348
marcelomorales <lmoral10@jhu.edu>
2021-05-13 21:31:18 -0400

    all cards are clickale and take you to a corresponding detail page about the card historical event

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -5,6 +5,9 @@ import Source from '../components/Source'
 import Carousel from 'react-bootstrap/Carousel' 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from '../images/cwb-logo-reverse-w-tagline 1.png';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
 
 
 
@@ -12,6 +15,13 @@ import logo from '../images/cwb-logo-reverse-w-tagline 1.png';
 import {
   firebase
 } from '../util';
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    maxWidth: 500,
+  },
+});
   
 
 class Details extends React.Component {
@@ -20,52 +30,54 @@ class Details extends React.Component {
   		super();
      }
      */
-	 //classes = useStyles();
+   //classes = useStyles();
+
+
 	 
 	render() {
 		return (
 
     <React.Fragment> 
-        <Carousel>
-    <Carousel.Item>
-      <img src={logo}  alt="logo"
-      />
-      <Carousel.Caption>
-        <h3>First slide label</h3>
-        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src="holder.js/800x400?text=Second slide&bg=282c34"
-        alt="Second slide"
-      />
+      <Carousel>
+        <Carousel.Item>
+          <img src={logo}  alt="logo"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={logo}  alt="logo"/>  
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src={logo}  alt="logo"/>
+        </Carousel.Item>
+      </Carousel>
 
-      <Carousel.Caption>
-        <h3>Second slide label</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src="holder.js/800x400?text=Third slide&bg=20232a"
-        alt="Third slide"
-      />
+        {/*Right now everything is hard coded but should be passed in as props in future */}
+      <div className="card-body">
+        <h1 className="title-event">Sofka Skipwith</h1>
+        <h2 className="title-location">London, England</h2>
+        <h2 className="title-date">3 Apr 1944</h2>
 
-      <Carousel.Caption>
-        <h3>Third slide label</h3>
-        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-  </Carousel>
+        <div className="row=title">
+          <h3 className="card-submitter">By Jane Doe</h3>
+          <h3 className="card-grade">Grade 8</h3>
+          <h3 className="submitter-location">Meadowvale PS</h3>
+        </div>
+
+        <br></br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+
+At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
 
 
+      </div>
    
       <Source/>
+
+
       </React.Fragment>
      
+
 			);
 	}
 }
