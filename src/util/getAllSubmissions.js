d8b7aef1b7c710c60ca14fff1af72da1472da253
elizabethhh <elizabethsong65@yahoo.com>
2021-05-27 21:08:59 -0400

    removed comments

--- src/util/getAllSubmissions.js
+++ src/util/getAllSubmissions.js
@@ -9,14 +9,11 @@ function getAllSubmissions(){
   var i = 0;
   db.collection('submissions').get().then((submissions) => {
     submissions.forEach((doc) => {
-      // documents[doc.id] = doc.data();
       submissionObjects[i] = new StudentSubmission("title", doc.data().date, doc.data().description, doc.data().images, doc.data().sources, doc.data().studentName, doc.data().submittedDate, "");
       i+=1;
 
   })});
   
-  // console.log(db.collection('testClass'));
-  console.log("hello world");
   console.log(documents);
   console.log(submissionObjects);
   console.log(submissionObjects.pop());
