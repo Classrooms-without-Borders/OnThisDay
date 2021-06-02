ac02cddf97e247da94432134259e5f8e75e09a74
elizabethhh <elizabethsong65@yahoo.com>
2021-06-02 14:46:54 -0400

    added all logos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from '../images/cwb-logo-reverse-w-tagline 1.png';
 import programInfo from '../images/jewish-federation-pitt.png';
 import facebook from '../images/facebook-logo.png';
+import insta from '../images/insta-logo.png';
+import twitter from '../images/twitter-logo.png';
 import '../styling/Footer.css';
 
 export function Footer() {
@@ -13,19 +15,25 @@ export function Footer() {
         <br />
         <ul><a href="https://classroomswithoutborders.org/" target="_blank" rel="noopener noreferrer">Website</a></ul>
         <ul><a href="https://classroomswithoutborders.org/donate/" target="_blank" rel="noopener noreferrer">Donate</a></ul>
+        
+          <a href="https://www.facebook.com/classroomswithoutborders">
+            <img src={facebook} alt="logo" hspace="20"/>
+          </a>
+          <a href="https://www.instagram.com/classroomswithoutborders/">
+              <img src={insta}  alt="logo" hspace="20" />
+          </a>
+          <a href="https://twitter.com/cwbpgh">
+              <img src={twitter}  alt="logo"  />
+            </a>
         </p></div>
         <div><img src={programInfo} alt="logo" height="100px"/></div>
         <div>
-          <a href="https://www.facebook.com/classroomswithoutborders">
-            <img src={facebook} alt="logo" />
-          </a>
+         
         </div>
         <div>
-          {/*
-            <a href="https://www.instagram.com/classroomswithoutborders/">
-              <img src={insta}  alt="logo"  />
-            </a>
-          */}
+          
+            
+         
         </div>
       </div>
     </div>
