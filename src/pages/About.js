e0a10de6197b61ada95eaf35c6cc375b85ab1220
Megan Lin <weitungl@andrew.cmu.edu>
2021-06-05 19:11:13 -0700

    form validations

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,6 +6,80 @@ import pic from '../images/CWB_Logo_KO_NoTag.png';
 import constants from '../styling/Constants';
 
 class About extends React.Component {
+	constructor(props) {
+		super(props);
+		this.formSubmit = this.formSubmit.bind(this);
+		this.state = {
+			fields: {},
+			errors: {}
+		}
+	}
+	handleChange(field, event) {
+		let fields = this.state.fields;
+		fields[field] = event.target.value;        
+		this.setState({fields});
+	}
+
+	handleValidation() {
+		let fields = this.state.fields;
+		let errors = {};
+		let formIsValid = true;
+
+		//Name
+		if(!fields["name"]){
+				formIsValid = false;
+				errors["name"] = "Cannot be empty";
+		}
+
+		if(typeof fields["name"] !== "undefined"){
+				if(!fields["name"].match(/^[a-zA-Z]+$/)){
+					formIsValid = false;
+					errors["name"] = "Only letters";
+				}        
+		}
+		//Email
+		if(!fields["email"]){
+			formIsValid = false;
+			errors["email"] = "Cannot be empty";
+		}
+
+	 	if(typeof fields["email"] !== "undefined"){
+			let lastAtPos = fields["email"].lastIndexOf('@');
+			let lastDotPos = fields["email"].lastIndexOf('.');
+
+			if (!(lastAtPos < lastDotPos && lastAtPos > 0 && fields["email"].indexOf('@@') == -1 && lastDotPos > 2 && (fields["email"].length - lastDotPos) > 2)) {
+				 formIsValid = false;
+				 errors["email"] = "Email is not valid";
+			 }
+		}  
+
+		this.setState({errors: errors});
+		return formIsValid;
+	}
+
+	contactSubmit(e){
+		e.preventDefault();
+
+		if(this.handleValidation()){
+			 alert("Form submitted");
+		}else{
+			 alert("Form has errors.")
+		}
+
+	}
+
+	formSubmit(event) {
+		event.preventDefault();
+		let fields = this.state.fields;
+
+
+		if(this.handleValidation()){
+			alert("Form submitted");
+		} else{
+			alert("Form has errors.")
+		}
+
+}
 	render() {
 		const header1 = {
 			fontFamily: '"Limelight", sans-serif',
