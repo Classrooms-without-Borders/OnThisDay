4858296cadc92386b44fc0d7f0978564bfdca731
kzhang01 <karinazhang1@gmail.com>
2021-09-26 23:42:30 -0400

    modified getAllVerified and StudentSubmission to include lat/lng

--- src/util/StudentSubmission.js
+++ src/util/StudentSubmission.js
@@ -4,12 +4,14 @@ Represents an individual student submission, similar to the submission collectio
 
 class StudentSubmission {
     constructor(
-        id, subjectName, location, eventDate, description,
+        id, subjectName, location, lat, lng, eventDate, description,
         images, sources, studentName, submitDate, className
     ) {
         this.id = id;
         this.subjectName = subjectName;
         this.location = location;
+        this.lat = lat;
+        this.lng = lng;
         this.eventDate = eventDate;
         this.description = description;
         this.images = images; // list of reference strings
